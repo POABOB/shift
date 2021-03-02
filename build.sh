6da319ba49d3b6f9c0d29cb6deb6af8937fc4272
YingSiang <zxc752166@gmail.com>
2021-03-03 00:57:55 +0800

    v1.6

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -r ./prd
 mkdir ./prd
 old=33
 # 在這個迴圈填入要產生的診所id，要用空白隔開，(33不能動，不然就抓不到id，33是公司的id)
-for clinicId in 33 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55
+for clinicId in 33 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56
 do
     sed -i "s/clinicId:$old,/clinicId:$clinicId,/g" ./src/components/Shift.vue
     npm run build
